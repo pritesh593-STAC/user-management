4285abc51df89c78ba520f22b42e2bd5680473e4
Pritesh Paikrao <pritesh9999d@gmail.com>
2026-02-13 16:28:00 +0530

    minimal server test

--- server/index.js
+++ server/index.js
@@ -1,29 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
 
 const app = express();
-
-
-app.use(cors());
-
-app.use(express.json());
-
-connectDB().catch(err => {
-  console.error("DB connection failed:", err.message);
-});
-
-
-const userRoutes = require("./routes/userRoutes");
-app.use("/api", userRoutes);
-
 
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
