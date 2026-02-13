8c60a73135253826d4601caa832e5bf926555793
Pritesh Paikrao <pritesh9999d@gmail.com>
2026-02-13 15:31:21 +0530

    new

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend.onrender.com/api",
+  baseURL: "https://user-management-1-f0ow.onrender.com/api",
 });
 
 export default API;
